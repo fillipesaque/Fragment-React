b0a692e55718dda0dd0095c2cd98e78302d3b840
Fillipe Saque <fillipesaque@gmail.com>
2023-10-04 18:58:22 -0300

    Fragment

---
+++ Fragment/src/App.jsx
@@ -0,0 +1,14 @@
+import { Fragment } from "react"
+import Components01 from "./Components/components01"
+
+
+function App() {
+  return(
+    <Fragment>
+        <h1>Lorem ipsum dolor sit, amet consectetur adipisicing.</h1>
+        <Components01></Components01>
+    </Fragment>
+  )
+}
+
+export default App
---
+++ Fragment/src/Components/Components01.jsx
@@ -0,0 +1,11 @@
+import styles from './Components01.module.css'
+
+function Components01 () {
+    return(
+        <div className={styles.pai}>
+            <h1>Lorem ipsum dolor sit.</h1>
+        </div>
+    )
+}
+
+export default Components01
